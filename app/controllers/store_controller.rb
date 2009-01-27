e43a7229e84f7d73af1fe240a7fe864fabddfbe0
Kristoffer <kristoffer@tassadar.local>
2009-01-27 17:39:07 +0100

    nu_då

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,11 +4,25 @@ class StoreController < ApplicationController
   end
 
   def add_to_cart
+    begin
+      product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      flash[:notice] = "Invalid Product"
+      redirect_to :action  => :index
+    else
     @cart = find_cart
     product = Product.find(params[:id])
     @cart.add_product(product)
+    end
   end
 
+  def empy_cart
+    session[:cart] = nil
+    flash[:notice] = "your cart is currently robbed"
+    redirect_to :action  => :index
+  end
+  
 private
   
   def find_cart
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,4 +8,13 @@ class Cart
   def add_product(product)
     @items << product
   end
+  
+  def add_product(product)
+    current_item = @items.find {|item| item.product == product}
+    if current_item
+      current_item.increment_quantity
+    else
+      @items << CartItem.new(product)
+    end
+  end
 end
